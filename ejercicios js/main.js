a6eaad184178385ab416f5aa0ee6bbae205d60c9
Nikohesu <nic3n1217@gmail.com>
2025-03-18 06:53:18 -0500

    se agregaron fuentes nuevas, se cambio el style de agency touts y se inicio a crear su main, falta terminar la logica y hacer pruevas

--- ejercicios js/main.js
+++ ejercicios js/main.js
@@ -3,30 +3,31 @@
 function cal () {
     let ciudad = document.getElementById("ciudad").value;
     let num_per = document.getElementById("num-personas").value;
-    num_per = num_per!=="" ? num_per : 0 ;
+    num_per = num_per!=="" ? (parseInt(num_per)<0? num_per=0:parseInt(num_per)) : 0 ;
     let tip_pago = document.getElementById("tipo-pago").value;  
     
     
-    let tot_com_p=tot_com_t=tot_aloj_p=tot_aloj_t=val_tiq_p=val_tiq_p_n=des_tiq_p=des_tiq_t=val_tiq_t_n=val_tiq_t=val_t_n=val_t_n_p=val_t_p=val_t=0;
+    let tot_com_p=tot_com_t=tot_aloj_p=tot_aloj_t=des_tiq_t=val_tiq_t_n=val_tiq_t=val_t_n=val_t_n_p=val_t_p=val_t=0;
+    let res = num_per+6;
+    console.log(res);
+    let viajes = [["Ciudad A",5,15000,100000,9000],
+                ["Ciudad B",4,12000,120000,11000],
+                ["Ciudad C",8,14000,110000,12000],
+                ["Ciudad D",6,17000,115000,10000]];
 
-    if (ciudad == "Ciudad A") {
-        console.log("cd a");
+    let inx = ciudad==viajes[0][0] ? 0:ciudad==viajes[1][0]?1:ciudad==viajes[2][0]?2:3 ;
 
-    }
-    else if (ciudad == "Ciudad B") {
-        console.log("cd b");
-
-    }
-    else if (ciudad == "Ciudad C") {
-        console.log("cd c");
-
-    }
-    else {
-        console.log("cd d");
-    }
+    tot_com_p = viajes[inx][1] * viajes[inx][4];
+    tot_com_t = tot_com_p*num_per;
+    tot_aloj_p = viajes[inx][1] * viajes[inx][2];
+    tot_aloj_t =tot_aloj_p*num_per;
+    val_tiq_p = viajes[inx][3];
+    val_tiq_t= viajes[inx][1] * viajes[inx][3];
+    des_tiq_t= ciudad==viajes[0][0] || ciudad==viajes[1][0];
 
 
-
+    console.log(des_tiq_t);
+  
 
 
 
