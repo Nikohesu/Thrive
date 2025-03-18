fe3b3742d4730488f7fb10f06c8e5e2d3674902a
Nikohesu <nic3n1217@gmail.com>
2025-03-18 11:58:56 -0500

    continuo en sena, suiguiente casa

--- ejercicios-js/main.js
+++ ejercicios-js/main.js
@@ -7,7 +7,7 @@ function cal () {
     let tip_pago = document.getElementById("tipo-pago").value;  
     
     
-    let tot_com_p=tot_com_t=tot_aloj_p=tot_aloj_t=des_tiq_t=val_tiq_t_n=val_tiq_t=val_t_n=val_t_n_p=val_t_p=val_t=0;
+    let tot_com_p=tot_com_t=tot_aloj_p=tot_aloj_t=des_tiq_t=des_tiq_l=val_tiq_t_n=val_tiq_t=val_t_n=val_t_n_p=val_t_p=val_t=0;
     let res = num_per+6;
     console.log(res);
     let viajes = [["Ciudad A",5,15000,100000,9000],
@@ -15,17 +15,20 @@ function cal () {
                 ["Ciudad C",8,14000,110000,12000],
                 ["Ciudad D",6,17000,115000,10000]];
 
-    let inx = ciudad==viajes[0][0] ? 0:ciudad==viajes[1][0]?1:ciudad==viajes[2][0]?2:3 ;
 
-    tot_com_p = viajes[inx][1] * viajes[inx][4];
+    tot_com_p = viajes[ciudad][1] * viajes[ciudad][4];
     tot_com_t = tot_com_p*num_per;
-    tot_aloj_p = viajes[inx][1] * viajes[inx][2];
+    tot_aloj_p = viajes[ciudad][1] * viajes[ciudad][2];
     tot_aloj_t =tot_aloj_p*num_per;
-    val_tiq_p = viajes[inx][3];
-    val_tiq_t= viajes[inx][1] * viajes[inx][3];
-    des_tiq_t= ciudad==viajes[0][0] || ciudad==viajes[1][0];
+    val_tiq_p = viajes[ciudad][3];
+    val_tiq_t= num_per * viajes[ciudad][3];
+    des_tiq_l =  ciudad == 0 || ciudad == 1 ? val_tiq_t*0.02  : ciudad==2 || ciudad==3 ? val_tiq_t*0.05:false;
+    des_tiq_t = tip_pago == "Efectivo" ? des_tiq_l+(val_tiq_t*0.04): des_tiq_t;
+        // 0 = Ciudad A, 1= Ciudad B, 2=Ciudad C, 3=Ciudad D
+     
 
-
+    console.log (tip_pago);
+    console.log(des_tiq_l);
     console.log(des_tiq_t);
   
 
